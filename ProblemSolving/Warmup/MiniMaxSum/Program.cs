ee98a7b4ed7cee982da5adb2a5de9641214162c9
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-18 14:08:37 +0200

    Setup a test case.

--- ProblemSolving/Warmup/MiniMaxSum/Program.cs
+++ ProblemSolving/Warmup/MiniMaxSum/Program.cs
@@ -1,31 +1,17 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Solution
 {
-
-    // Complete the miniMaxSum function below.
-    static void miniMaxSum(int[] arr)
+    static void MiniMaxSum(int[] arr)
     {
-
 
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
-        ;
-        miniMaxSum(arr);
+        var testData = "1 2 3 4 5";
+
+        int[] arr = Array.ConvertAll(testData.Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        MiniMaxSum(arr);
     }
 }
