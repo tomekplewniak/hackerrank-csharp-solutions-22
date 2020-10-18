be9e92083d9ef75100358eadb038df5ad91cb79f
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-18 14:04:38 +0200

    Problem: Plus Minus (#3)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Warmup/PlusMinus/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static void PlusMinus(int[] arr)
+    {
+        Console.WriteLine("{0:F6}", (double)arr.Count(x => x > 0) / arr.Count());
+        Console.WriteLine("{0:F6}", (double)arr.Count(x => x < 0) / arr.Count());
+        Console.WriteLine("{0:F6}", (double)arr.Count(x => x == 0) / arr.Count());
+    }
+
+    static void Main()
+    {
+        var testData = "-4 3 -9 0 4 1";
+
+        int[] arr = Array.ConvertAll(testData.Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        PlusMinus(arr);
+    }
+}
