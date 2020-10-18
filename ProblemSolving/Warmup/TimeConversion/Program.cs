5449ff758c017923c948be722c4b677e2ae22392
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-18 14:17:07 +0200

    Initial a problem.

---
+++ ProblemSolving/Warmup/TimeConversion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the timeConversion function below.
+     */
+    static string timeConversion(string s)
+    {
+        /*
+         * Write your code here.
+         */
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = timeConversion(s);
+
+        tw.WriteLine(result);
+
+        tw.Flush();
+        tw.Close();
+    }
+}
