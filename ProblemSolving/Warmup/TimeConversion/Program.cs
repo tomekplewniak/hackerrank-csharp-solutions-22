58b963dba4d0d19d666ce584c962c55e9a3dc239
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-18 14:28:48 +0200

    Problem: Time Conversion (#5)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Warmup/TimeConversion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Solution
+{
+    static string TimeConversion(string s)
+    {
+        var sArray = s.Split(':');
+
+        var hh = sArray[0];
+        var mm = sArray[1];
+        var ss = sArray[2].Remove(2);
+
+        if (s.Contains("AM"))
+        {
+            if (hh.Contains("12"))
+            {
+                hh = "00";
+            }
+
+            return $"{hh}:{mm}:{ss}";
+        }
+        else
+        {
+            if (hh.Contains("12") == false)
+            {
+                hh = (Convert.ToInt32(hh) + 12).ToString();
+            }
+
+            return $"{hh}:{mm}:{ss}";
+        }
+    }
+
+    static void Main()
+    {
+        string s = "07:05:45PM";
+
+        string result = TimeConversion(s);
+        
+        Console.WriteLine(result);
+    }
+}
