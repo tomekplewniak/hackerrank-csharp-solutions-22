846b425c3f7d485e89af322c2f962bff97aa85bd
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-18 14:14:37 +0200

    Problem: Mini-Max Sum (#4)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Warmup/MiniMaxSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static void MiniMaxSum(int[] arr)
+    {
+        long sum = 0;
+
+        foreach (var a in arr)
+        {
+            sum += a;
+        }
+
+        Console.WriteLine("{0} {1}", sum - arr.Max(), sum - arr.Min());
+    }
+
+    static void Main()
+    {
+        var testData = "1 2 3 4 5";
+
+        int[] arr = Array.ConvertAll(testData.Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        MiniMaxSum(arr);
+    }
+}
