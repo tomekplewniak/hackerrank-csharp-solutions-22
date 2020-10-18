b1fb252bfb00443c79b740566abcedfa590df542
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-18 13:48:34 +0200

    Problem: Simple Array Sum (#1)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Warmup/SimpleArraySum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static int SimpleArraySum(int[] ar)
+    {
+        return ar.Sum();
+    }
+
+    static void Main()
+    {
+        var testData = "1 2 3 4 10 11";
+
+        int[] ar = Array.ConvertAll(testData.Split(' '), arTemp => Convert.ToInt32(arTemp));
+        int result = SimpleArraySum(ar);
+
+        Console.WriteLine(result);
+    }
+}
