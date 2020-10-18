d04e8f164839620d359f7e9e5763fd36f5bb1e26
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-18 14:27:38 +0200

    Solve the problem.

--- ProblemSolving/Warmup/TimeConversion/Program.cs
+++ ProblemSolving/Warmup/TimeConversion/Program.cs
@@ -4,7 +4,30 @@ class Solution
 {
     static string TimeConversion(string s)
     {
+        var sArray = s.Split(':');
 
+        var hh = sArray[0];
+        var mm = sArray[1];
+        var ss = sArray[2].Remove(2);
+
+        if (s.Contains("AM"))
+        {
+            if (hh.Contains("12"))
+            {
+                hh = "00";
+            }
+
+            return $"{hh}:{mm}:{ss}";
+        }
+        else
+        {
+            if (hh.Contains("12") == false)
+            {
+                hh = (Convert.ToInt32(hh) + 12).ToString();
+            }
+
+            return $"{hh}:{mm}:{ss}";
+        }
     }
 
     static void Main()
