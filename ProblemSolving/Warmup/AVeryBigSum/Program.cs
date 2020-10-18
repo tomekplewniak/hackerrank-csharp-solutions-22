9fb8ac06767d64280419501dc36f47ac7c4778ca
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-18 13:55:59 +0200

    Problem: A Very Big Sum (#2)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Warmup/AVeryBigSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static long AVeryBigSum(long[] ar)
+    {
+        return ar.Sum();
+    }
+
+    static void Main()
+    {
+        var testData = "1000000001 1000000002 1000000003 1000000004 1000000005";
+
+        long[] ar = Array.ConvertAll(testData.Split(' '), arTemp => Convert.ToInt64(arTemp));
+        long result = AVeryBigSum(ar);
+
+        Console.WriteLine(result);
+    }
+}
