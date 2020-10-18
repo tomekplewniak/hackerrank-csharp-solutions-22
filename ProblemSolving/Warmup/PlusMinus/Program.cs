a6e0fa6f57b42a3996620125cc00ba7bb94e967e
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-18 13:58:26 +0200

    Initial a problem.

---
+++ ProblemSolving/Warmup/PlusMinus/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr)
+    {
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        plusMinus(arr);
+    }
+}
