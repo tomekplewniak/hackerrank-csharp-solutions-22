753b795939ac28ae897659233fd4e5d2538bb489
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-18 13:59:49 +0200

    Setup a test case.

--- ProblemSolving/Warmup/PlusMinus/Program.cs
+++ ProblemSolving/Warmup/PlusMinus/Program.cs
@@ -1,33 +1,17 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Solution
 {
-
-    // Complete the plusMinus function below.
-    static void plusMinus(int[] arr)
+    static void PlusMinus(int[] arr)
     {
-
 
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        int n = Convert.ToInt32(Console.ReadLine());
+        var testData = "-4 3 -9 0 4 1";
 
-        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
-        ;
-        plusMinus(arr);
+        int[] arr = Array.ConvertAll(testData.Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        PlusMinus(arr);
     }
 }
