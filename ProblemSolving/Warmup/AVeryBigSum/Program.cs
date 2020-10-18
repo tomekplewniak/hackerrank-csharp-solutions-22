4ded4d10f30535e1860482a00204d691e87cc6d9
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-18 13:53:52 +0200

    Setup a test case.

--- ProblemSolving/Warmup/AVeryBigSum/Program.cs
+++ ProblemSolving/Warmup/AVeryBigSum/Program.cs
@@ -1,40 +1,19 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Solution
 {
-
-    // Complete the aVeryBigSum function below.
-    static long aVeryBigSum(long[] ar)
+    static long AVeryBigSum(long[] ar)
     {
-
 
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        var testData = "1000000001 1000000002 1000000003 1000000004 1000000005";
 
-        int arCount = Convert.ToInt32(Console.ReadLine());
+        long[] ar = Array.ConvertAll(testData.Split(' '), arTemp => Convert.ToInt64(arTemp));
+        long result = AVeryBigSum(ar);
 
-        long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp))
-        ;
-        long result = aVeryBigSum(ar);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
+        Console.WriteLine(result);
     }
 }
