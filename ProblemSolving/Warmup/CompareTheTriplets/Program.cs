61dad9aa611ee4194f9df53272d1d16f6eec3f53
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-23 11:06:50 +0200

    Problem: Compare the Triplets (#6)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Warmup/CompareTheTriplets/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+class Solution
+{
+    static List<int> CompareTriplets(List<int> a, List<int> b)
+    {
+        var aCount = a.Count();
+        var bCount = a.Count();
+
+        var alice = 0;
+        var bob = 0;
+
+        if (aCount >= 1 || aCount <= 100
+            || bCount >= 1 || bCount <= 100
+            && aCount == bCount)
+        {
+            for (int i = 0; i < aCount; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    alice++;
+                }
+
+                if (a[i] < b[i])
+                {
+                    bob++;
+                }
+            }
+
+            return new List<int> { alice, bob };
+        }
+        else
+        {
+            throw new ArgumentException("Wrong input data.");
+        }
+    }
+
+    static void Main()
+    {
+        var a = new List<int>() { 5, 6, 7 };
+
+        var b = new List<int>() { 3, 6, 10 };
+
+        List<int> result = CompareTriplets(a, b);
+
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
