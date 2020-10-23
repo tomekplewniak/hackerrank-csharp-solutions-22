1064a693c0bcc235fc1f5bd602e75c739ab71321
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-23 11:40:02 +0200

    Problem: Staircase (#8)
    * Initial a problem.
    
    * Solve the problem.

---
+++ ProblemSolving/Warmup/Staircase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Solution
+{
+    static void staircase(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i + j >= n - 1)
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        staircase(n);
+    }
+}
