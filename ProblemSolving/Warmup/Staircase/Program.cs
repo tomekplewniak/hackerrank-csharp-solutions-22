aaaa0c1e245a64be64053231dff7a45ba574faa7
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-23 11:39:11 +0200

    Solve the problem.

--- ProblemSolving/Warmup/Staircase/Program.cs
+++ ProblemSolving/Warmup/Staircase/Program.cs
@@ -1,25 +1,24 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Solution
 {
-
-    // Complete the staircase function below.
     static void staircase(int n)
     {
-
-
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i + j >= n - 1)
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
     }
 
     static void Main(string[] args)
