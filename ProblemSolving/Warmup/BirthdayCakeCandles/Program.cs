0575b1a411c6a0dcaaaff03137d612a6109e34f8
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-23 11:25:26 +0200

    Setup a test case.

--- ProblemSolving/Warmup/BirthdayCakeCandles/Program.cs
+++ ProblemSolving/Warmup/BirthdayCakeCandles/Program.cs
@@ -1,49 +1,22 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
+﻿using System.Collections.Generic;
 using System;
 
 class Result
 {
-
-    /*
-     * Complete the 'birthdayCakeCandles' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY candles as parameter.
-     */
-
-    public static int birthdayCakeCandles(List<int> candles)
+    public static int BirthdayCakeCandles(List<int> candles)
     {
 
     }
-
 }
 
 class Solution
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        var candles = new List<int>() { 3, 2, 1, 3 };
 
-        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+        int result = Result.BirthdayCakeCandles(candles);
 
-        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
-
-        int result = Result.birthdayCakeCandles(candles);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
+        Console.WriteLine(result);
     }
 }
