d5cf4c1d8247f09a3bc95d495600c0e3942e6b57
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-23 12:05:28 +0200

    Setup a test case.

--- ProblemSolving/Implementation/GradingStudents/Program.cs
+++ ProblemSolving/Implementation/GradingStudents/Program.cs
@@ -1,55 +1,22 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
+﻿using System.Collections.Generic;
 using System;
 
 class Result
 {
-
-    /*
-     * Complete the 'gradingStudents' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts INTEGER_ARRAY grades as parameter.
-     */
-
-    public static List<int> gradingStudents(List<int> grades)
+    public static List<int> GradingStudents(List<int> grades)
     {
 
     }
-
 }
 
 class Solution
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        List<int> grades = new List<int>() { 73, 67, 38, 33 };
 
-        int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+        List<int> result = Result.GradingStudents(grades);
 
-        List<int> grades = new List<int>();
-
-        for (int i = 0; i < gradesCount; i++)
-        {
-            int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
-            grades.Add(gradesItem);
-        }
-
-        List<int> result = Result.gradingStudents(grades);
-
-        textWriter.WriteLine(String.Join("\n", result));
-
-        textWriter.Flush();
-        textWriter.Close();
+        Console.WriteLine(String.Join("\n", result));
     }
 }
