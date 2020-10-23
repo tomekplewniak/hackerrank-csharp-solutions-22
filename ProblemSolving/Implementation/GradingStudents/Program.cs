0001ddac7b529518ff6887d7d70b1ed31cab6df6
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-23 12:07:17 +0200

    Problem: Grading Students (#10)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Implementation/GradingStudents/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System;
+
+class Result
+{
+    public static List<int> GradingStudents(List<int> grades)
+    {
+        var result = new List<int>();
+
+        for (int i = 0; i < grades.Count; i++)
+        {
+            if (grades[i] >= 38)
+            {
+                if ((grades[i] % 5) > 2)
+                {
+                    grades[i] += 5 - (grades[i] % 5);
+                }
+            }
+
+            result.Add(grades[i]);
+        }
+
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main()
+    {
+        List<int> grades = new List<int>() { 73, 67, 38, 33 };
+
+        List<int> result = Result.GradingStudents(grades);
+
+        Console.WriteLine(String.Join("\n", result));
+    }
+}
