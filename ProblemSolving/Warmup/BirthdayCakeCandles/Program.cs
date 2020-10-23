da94058b3968b0da887c5550caf7ed595aeabe6d
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-23 11:30:32 +0200

    Problem: Birthday Cake Candles (#7)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Warmup/BirthdayCakeCandles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+class Result
+{
+    public static int BirthdayCakeCandles(List<int> candles)
+    {
+        var max = candles.Max();
+
+        return candles.Count(val => val == max);
+    }
+}
+
+class Solution
+{
+    public static void Main()
+    {
+        var candles = new List<int>() { 3, 2, 1, 3 };
+
+        int result = Result.BirthdayCakeCandles(candles);
+
+        Console.WriteLine(result);
+    }
+}
