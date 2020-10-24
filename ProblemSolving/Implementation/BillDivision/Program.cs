91319eeef5ff9cdecfa0d2d5fbd27630f55d17a3
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-24 16:58:22 +0200

    Solve the problem.

--- ProblemSolving/Implementation/BillDivision/Program.cs
+++ ProblemSolving/Implementation/BillDivision/Program.cs
@@ -1,10 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Solution
 {
     static void BonAppetit(List<int> bill, int k, int b)
     {
+        var sum = bill.Sum() - bill[k];
 
+        if ((sum /2 ) == b)
+        {
+            Console.WriteLine("Bon Appetit");
+        }
+        else
+        {
+            Console.WriteLine($"{b - sum / 2}");
+        }
     }
 
     static void Main()
