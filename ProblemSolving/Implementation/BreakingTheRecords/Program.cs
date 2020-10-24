e10026e9e4628d5cecca37ee93bba25166142577
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-24 16:42:44 +0200

    Problem: Breaking the Records (#12)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Implementation/BreakingTheRecords/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Solution
+{
+    static int[] BreakingRecords(int[] scores)
+    {
+        var best = scores[0];
+        var bestCouner = 0;
+
+        var worst = scores[0];
+        var worstCounter = 0;
+
+        foreach (var i in scores)
+        {
+            if (i > best)
+            {
+                best = i;
+                bestCouner++;
+            }
+
+            if (i < worst)
+            {
+                worst = i;
+                worstCounter++;
+            }
+        }
+
+        return new int[] { bestCouner, worstCounter };
+    }
+
+    static void Main()
+    {
+        var testData = "3 4 21 36 10 28 35 5 24 42";
+
+        int[] scores = Array.ConvertAll(testData.Split(' '), scoresTemp => Convert.ToInt32(scoresTemp));
+        int[] result = BreakingRecords(scores);
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
