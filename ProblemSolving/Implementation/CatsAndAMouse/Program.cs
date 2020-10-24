83e0e5353cee1c2f2b6aec18a9bbafbf04cc0b4d
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-24 17:29:25 +0200

    Problem: Cats and a Mouse (#15)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Implementation/CatsAndAMouse/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    static string CatAndMouse(int x, int y, int z)
+    {
+        if (Math.Abs(x - z) == Math.Abs(y - z))
+        {
+            return "Mouse C";
+        }
+        else
+        {
+            if (Math.Abs(x - z) < Math.Abs(y - z))
+            {
+                return "Cat A";
+            }
+            else
+            {
+                return "Cat B";
+            }
+        }
+    }
+
+    static void Main()
+    {
+        var testData = new List<List<int>>()
+        {
+            new List<int>(){1, 2, 3 },
+            new List<int>(){1, 3, 2 }
+        };
+
+        for (int qItr = 0; qItr < testData.Count; qItr++)
+        {
+            int x = testData[qItr][0];
+
+            int y = testData[qItr][1];
+
+            int z = testData[qItr][2];
+
+            string result = CatAndMouse(x, y, z);
+
+            Console.WriteLine(result);
+        }
+    }
+}
