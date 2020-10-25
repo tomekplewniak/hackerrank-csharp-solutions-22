c2eb86324cbe537c8f0219165e533fe41d9ee3db
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-25 09:56:06 +0100

    Problem: CamelCase (#16)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.
    
    * Solve the problem using LINQ.

---
+++ ProblemSolving/Strings/CamelCase/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static int Camelcase(string s)
+    {
+        var wordsCounter = 1;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (Char.IsUpper(s[i]))
+            {
+                wordsCounter++;
+            }
+        }
+
+        return wordsCounter;
+    }
+
+    static int CamelcaseByLinq(string s)
+    {
+        return s.Count(c => Char.IsUpper(c)) + 1;
+    }
+
+    static void Main()
+    {
+        string s = "saveChangesInTheEditor";
+
+        Console.WriteLine("Solve by for loop:");
+        int result = Camelcase(s);
+        Console.WriteLine(result);
+
+        Console.WriteLine("Solve by LINQ:");
+        int secondResult = CamelcaseByLinq(s);
+
+        Console.WriteLine(secondResult);
+    }
+}
