eda4232b4360a562f830fce80dbbc65c509021ba
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-25 10:01:59 +0100

    Setup a test case.

--- ProblemSolving/Strings/HackerRankInAString/Program.cs
+++ ProblemSolving/Strings/HackerRankInAString/Program.cs
@@ -1,43 +1,28 @@
-﻿using System.CodeDom.Compiler;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
 
 class Solution
 {
-
-    // Complete the hackerrankInString function below.
-    static string hackerrankInString(string s)
+    static string HackerrankInString(string s)
     {
-
 
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int q = Convert.ToInt32(Console.ReadLine());
-
-        for (int qItr = 0; qItr < q; qItr++)
+        var testData = new List<string>()
         {
-            string s = Console.ReadLine();
+            "hhaacckkekraraannk",
+            "rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktjjt"
+        };
 
-            string result = hackerrankInString(s);
+        for (int qItr = 0; qItr < testData.Count; qItr++)
+        {
+            string s = testData[qItr];
 
-            textWriter.WriteLine(result);
+            string result = HackerrankInString(s);
+
+            Console.WriteLine(result);
         }
-
-        textWriter.Flush();
-        textWriter.Close();
     }
 }
