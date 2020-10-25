413a9c10dde5813bfd429caa15885502f143500a
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-25 10:19:10 +0100

    Setup a test case.

--- ProblemSolving/Sorting/IntroToTutorialChallenges/Program.cs
+++ ProblemSolving/Sorting/IntroToTutorialChallenges/Program.cs
@@ -1,42 +1,20 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Solution
 {
-
-    // Complete the introTutorial function below.
-    static int introTutorial(int V, int[] arr)
+    static int IntroTutorial(int V, int[] arr)
     {
-
 
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        int V = 4;
 
-        int V = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[] { 1, 4, 5, 7, 9, 12 };
 
-        int n = Convert.ToInt32(Console.ReadLine());
+        int result = IntroTutorial(V, arr);
 
-        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
-        ;
-        int result = introTutorial(V, arr);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
+        Console.WriteLine(result);
     }
 }
