86f3c53a94239856bec49baf58ccf8e4f5c37c48
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-25 10:32:37 +0100

    Initial a problem.

---
+++ ProblemSolving/Sorting/BigSorting/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the bigSorting function below.
+    static string[] bigSorting(string[] unsorted)
+    {
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string[] unsorted = new string[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            string unsortedItem = Console.ReadLine();
+            unsorted[i] = unsortedItem;
+        }
+
+        string[] result = bigSorting(unsorted);
+
+        textWriter.WriteLine(string.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
