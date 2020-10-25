5a4fe68f24c85bb32a3333ec2b507d5d68bdd62d
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-25 10:30:01 +0100

    Problem: Intro to Tutorial Challenges (#18)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Sorting/IntroToTutorialChallenges/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static int IntroTutorial(int V, int[] arr)
+    {
+        return Array.IndexOf(arr, V);
+    }
+
+    static void Main()
+    {
+        int V = 4;
+
+        int[] arr = new int[] { 1, 4, 5, 7, 9, 12 };
+
+        int result = IntroTutorial(V, arr);
+
+        Console.WriteLine(result);
+    }
+}
