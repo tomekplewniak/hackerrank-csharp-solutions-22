4dfe0cf095fc4c742449adb745a276e5fddef145
Tomek Plewniak <tomekplewniak@gmail.com>
2020-10-25 10:09:40 +0100

    Solve the problem.

--- ProblemSolving/Strings/HackerRankInAString/Program.cs
+++ ProblemSolving/Strings/HackerRankInAString/Program.cs
@@ -5,7 +5,22 @@ class Solution
 {
     static string HackerrankInString(string s)
     {
+        var hackerrank = new Queue<char>("hackerrank".ToCharArray());
 
+        foreach (var c in s)
+        {
+            if (c == hackerrank.Peek())
+            {
+                hackerrank.Dequeue();
+            }
+
+            if (hackerrank.Count == 0)
+            {
+                return "YES";
+            }
+        }
+
+        return "NO";
     }
 
     static void Main()
