aef3138c0884a87e746bb302363a5036f837ecdc
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-25 10:10:29 +0100

    Problem: HackerRank in a String (#17)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Strings/HackerRankInAString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    static string HackerrankInString(string s)
+    {
+        var hackerrank = new Queue<char>("hackerrank".ToCharArray());
+
+        foreach (var c in s)
+        {
+            if (c == hackerrank.Peek())
+            {
+                hackerrank.Dequeue();
+            }
+
+            if (hackerrank.Count == 0)
+            {
+                return "YES";
+            }
+        }
+
+        return "NO";
+    }
+
+    static void Main()
+    {
+        var testData = new List<string>()
+        {
+            "hhaacckkekraraannk",
+            "rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktjjt"
+        };
+
+        for (int qItr = 0; qItr < testData.Count; qItr++)
+        {
+            string s = testData[qItr];
+
+            string result = HackerrankInString(s);
+
+            Console.WriteLine(result);
+        }
+    }
+}
