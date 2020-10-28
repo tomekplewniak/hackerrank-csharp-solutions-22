508ea6da316abab37a70c83fe52551f353a40054
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-10-28 13:11:06 +0100

    Problem: Pangrams (#20)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Strings/Pangrams/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    static string Pangrams(string s)
+    {
+        List<char> alphabet = new List<char>("abcdefghijklmnopqrstuvwxyz".ToCharArray());
+
+        foreach (var c in s)
+        {
+            if (alphabet.Contains(Char.ToLower(c)))
+            {
+                alphabet.Remove(Char.ToLower(c));
+            }
+        }
+
+        if (alphabet.Count == 0)
+        {
+            return "pangram";
+        }
+        else
+        {
+            return "not pangram";
+        }
+    }
+
+    static void Main()
+    {
+        string s = "We promptly judged antique ivory buckles for the next prize";
+
+        string result = Pangrams(s);
+
+        Console.WriteLine(result);
+    }
+}
