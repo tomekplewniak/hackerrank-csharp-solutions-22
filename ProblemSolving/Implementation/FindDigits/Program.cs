2f2cdc1eb988a05c15193c721137f846e76b19b4
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-11-01 17:39:57 +0100

    Problem: Find Digits (#21)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Implementation/FindDigits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    static int FindDigits(int n)
+    {
+        int res = n;
+        int c = 0;
+        while (res > 0)
+        {
+            int d = res % 10;
+            if (d != 0 && n % d == 0)
+                c++;
+            res /= 10;
+        }
+        return c;
+    }
+
+    static void Main()
+    {
+        List<int> testData = new List<int>() { 12, 1012 };
+
+        for (int tItr = 0; tItr < testData.Count; tItr++)
+        {
+            int result = FindDigits(testData[tItr]);
+
+            Console.WriteLine(result);
+        }
+    }
+}
