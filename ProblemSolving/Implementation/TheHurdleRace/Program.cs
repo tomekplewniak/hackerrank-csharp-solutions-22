5678c68714d0a96fa605365ef135a5236af103d1
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-11-07 12:51:54 +0100

    Problem: The Hurdle Race (#23)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Implementation/TheHurdleRace/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static int HurdleRace(int k, int[] height)
+    {
+        if (height.Max() - k > 0)
+        {
+            return height.Max() - k;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    static void Main()
+    {
+        int k = 4;
+
+        int[] height = new int[] { 1, 6, 3, 5, 2 };
+
+        int result = HurdleRace(k, height);
+
+        Console.WriteLine(result);
+    }
+}
