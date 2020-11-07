6432406707944f707ab969d8f7b78578f2bb315b
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-11-07 12:41:24 +0100

    Problem: Viral Advertising (#22)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Implementation/ViralAdvertising/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Solution
+{
+    static int ViralAdvertising(int n)
+    {
+        var likePerDay = 0;
+        var person = 5;
+
+        var allLikes = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            likePerDay = person / 2;
+
+            person = likePerDay * 3;
+
+            allLikes += likePerDay;
+        }
+
+        return allLikes;
+    }
+
+    static void Main()
+    {
+        int n = 3;
+
+        int result = ViralAdvertising(n);
+
+        Console.WriteLine(result);
+    }
+}
