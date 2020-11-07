10674f33e9095d8014e9f33470887d3824670d39
Tomek Plewniak <tomekplewniak@gmail.com>
2020-11-07 12:46:44 +0100

    Setup a test case.

--- ProblemSolving/Implementation/TheHurdleRace/Program.cs
+++ ProblemSolving/Implementation/TheHurdleRace/Program.cs
@@ -1,44 +1,20 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Solution
 {
-
-    // Complete the hurdleRace function below.
-    static int hurdleRace(int k, int[] height)
+    static int HurdleRace(int k, int[] height)
     {
-
 
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        int k = 4;
 
-        string[] nk = Console.ReadLine().Split(' ');
+        int[] height = new int[] { 1, 6, 3, 5, 2 };
 
-        int n = Convert.ToInt32(nk[0]);
+        int result = HurdleRace(k, height);
 
-        int k = Convert.ToInt32(nk[1]);
-
-        int[] height = Array.ConvertAll(Console.ReadLine().Split(' '), heightTemp => Convert.ToInt32(heightTemp))
-        ;
-        int result = hurdleRace(k, height);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
+        Console.WriteLine(result);
     }
 }
