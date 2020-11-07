415ace9614e4323352f891ee295c34d112eb0ad4
Tomek Plewniak <tomekplewniak@gmail.com>
2020-11-07 12:30:14 +0100

    Initial a problem.

---
+++ ProblemSolving/Implementation/ViralAdvertising/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n)
+    {
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = viralAdvertising(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
