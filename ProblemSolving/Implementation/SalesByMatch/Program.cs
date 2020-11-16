6c00dcc7f20f2f61b2b33eaac3171b16af62c520
Tomek Plewniak <tomekplewniak@gmail.com>
2020-11-16 09:59:47 +0100

    Setup a test case.

--- ProblemSolving/Implementation/SalesByMatch/Program.cs
+++ ProblemSolving/Implementation/SalesByMatch/Program.cs
@@ -1,40 +1,20 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Solution
 {
-
-    // Complete the sockMerchant function below.
-    static int sockMerchant(int n, int[] ar)
+    static int SockMerchant(int n, int[] ar)
     {
-
 
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        var n = 9;
+        var testData = "10 20 20 10 10 30 50 10 20";
+        
+        int[] ar = Array.ConvertAll(testData.Split(' '), arTemp => Convert.ToInt32(arTemp));
+        int result = SockMerchant(n, ar);
 
-        int n = Convert.ToInt32(Console.ReadLine());
-
-        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
-        ;
-        int result = sockMerchant(n, ar);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
+        Console.WriteLine(result);
     }
 }
