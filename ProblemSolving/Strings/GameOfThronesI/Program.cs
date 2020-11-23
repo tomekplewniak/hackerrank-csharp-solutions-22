84fc94790f0e4d5a61b21795898b228959ac914d
Tomek Plewniak <tomekplewniak@gmail.com>
2020-11-23 21:32:01 +0100

    Solve the problem.

--- ProblemSolving/Strings/GameOfThronesI/Program.cs
+++ ProblemSolving/Strings/GameOfThronesI/Program.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Solution
 {
     static string GameOfThrones(string s)
     {
+        HashSet<char> oddLetters = new HashSet<char>();
 
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (oddLetters.Contains(s[i]))
+            {
+                oddLetters.Remove(s[i]);
+            }
+            else
+            {
+                oddLetters.Add(s[i]);
+            }
+        }
+
+        if (s.Length % 2 == 0)
+        {
+            return oddLetters.Count == 0 ? "YES" : "NO";
+        }
+        else
+        {
+            return oddLetters.Count == 1 ? "YES" : "NO";
+        }
     }
 
     static void Main()
