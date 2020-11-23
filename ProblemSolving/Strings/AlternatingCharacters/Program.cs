3afca501d1215a44c8b89e98e15fc0cd87786109
Tomek Plewniak <tomekplewniak@gmail.com>
2020-11-23 20:30:24 +0100

    Setup a test case.

--- ProblemSolving/Strings/AlternatingCharacters/Program.cs
+++ ProblemSolving/Strings/AlternatingCharacters/Program.cs
@@ -1,43 +1,18 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Solution
 {
-
-    // Complete the alternatingCharacters function below.
-    static int alternatingCharacters(string s)
+    static int AlternatingCharacters(string s)
     {
-
 
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        var testData = "AAABBB";
 
-        int q = Convert.ToInt32(Console.ReadLine());
+        int result = AlternatingCharacters(testData);
 
-        for (int qItr = 0; qItr < q; qItr++)
-        {
-            string s = Console.ReadLine();
-
-            int result = alternatingCharacters(s);
-
-            textWriter.WriteLine(result);
-        }
-
-        textWriter.Flush();
-        textWriter.Close();
+        Console.WriteLine(result);
     }
 }
