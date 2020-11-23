5e4f5c96d2b686cd8fe1b35d9d82165e72730ebf
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-11-23 20:37:45 +0100

    Problem: Alternating Characters (#25)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Strings/AlternatingCharacters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Solution
+{
+    static int AlternatingCharacters(string s)
+    {
+        var deletions = 0;
+
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            if (s[i] == s[i+1])
+            {
+                deletions++;
+            }
+        }
+
+        return deletions;
+    }
+
+    static void Main()
+    {
+        var testData = "AAABBB";
+
+        int result = AlternatingCharacters(testData);
+
+        Console.WriteLine(result);
+    }
+}
