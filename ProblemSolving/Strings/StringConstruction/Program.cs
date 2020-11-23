fabe01cde1f2a8f6bca95144aecb836e7aac360c
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2020-11-23 20:51:31 +0100

    Problem: String Construction (#26)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Strings/StringConstruction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static int StringConstruction(string s)
+    {
+        return s.Distinct().Count();
+    }
+
+    static void Main()
+    {
+        var testData = "abab";
+
+        int result = StringConstruction(testData);
+
+        Console.WriteLine(result);
+    }
+}
