4da1cd54e148debb7cb3f6af2f3221ea54097743
Tomek Plewniak <tomekplewniak@gmail.com>
2020-11-23 20:36:02 +0100

    Solve the problem.

--- ProblemSolving/Strings/AlternatingCharacters/Program.cs
+++ ProblemSolving/Strings/AlternatingCharacters/Program.cs
@@ -4,7 +4,17 @@ class Solution
 {
     static int AlternatingCharacters(string s)
     {
+        var deletions = 0;
 
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            if (s[i] == s[i+1])
+            {
+                deletions++;
+            }
+        }
+
+        return deletions;
     }
 
     static void Main()
