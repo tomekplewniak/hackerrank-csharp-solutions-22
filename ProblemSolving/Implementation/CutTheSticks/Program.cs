2f77d499fd76ee29f83d171c0f9785fe1f3a18ff
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2021-01-31 14:30:57 +0100

    Problem: Cut the sticks (#28)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Implementation/CutTheSticks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static int[] CutTheSticks(int[] arr)
+    {
+        var result = new List<int>();
+        while (arr.Length > 0)
+        {
+            result.Add(arr.Length);
+
+            arr = arr.Where(val => val != arr.Min()).ToArray();
+        }
+
+        return result.ToArray();
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = new int[] { 1, 2, 3, 4, 3, 3, 2, 1 };
+        
+        int[] result = CutTheSticks(arr);
+
+        Console.WriteLine(string.Join("\n", result));
+    }
+}
