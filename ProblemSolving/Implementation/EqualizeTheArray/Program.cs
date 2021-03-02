d7994ba9cc27017a5cf9dcd0e91fa347d7b61674
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2021-03-02 14:51:49 +0100

    Problem: Equalize the Array (#30)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Implementation/EqualizeTheArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static int EqualizeArray(int[] arr)
+    {
+        var groupedList = from l in arr
+                          group l by l into grp
+                          orderby grp.Count() descending
+                          select new { Value = grp.Key, Count = grp.Count() };
+
+        var offtenValue = groupedList.First().Value;
+                  
+        return arr.Count(i => i != offtenValue);
+    }
+
+    static void Main()
+    {
+        var arr = new int[] { 3, 3, 2, 1, 3 };
+
+        int result = EqualizeArray(arr);
+
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
