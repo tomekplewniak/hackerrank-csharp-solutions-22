1063532a7f7670d597e1d392a7f6276108db5c2b
Tomek Plewniak <tomekplewniak@gmail.com>
2021-03-02 13:44:41 +0100

    Solve the problem.

--- ProblemSolving/Implementation/CavityMap/BeautifulDaysAtTheMovies/Program.cs
+++ ProblemSolving/Implementation/CavityMap/BeautifulDaysAtTheMovies/Program.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 class Solution
 {
     static int BeautifulDays(int i, int j, int k)
     {
+        var beautifulDaysCounter = 0;
 
+        if (i < j)
+        {
+            for (int dayNumb = i; dayNumb <= j; dayNumb++)
+            {
+                var reversedDayNumb = Reverse(dayNumb);
+
+                if ((dayNumb - reversedDayNumb) % k == 0)
+                {
+                    beautifulDaysCounter++;
+                }
+            }
+        }
+
+        return beautifulDaysCounter;
+    }
+
+    private static int Reverse(int number)
+    {
+        if (number.ToString().Length > 1)
+        {
+            var day = number.ToString().Reverse().ToArray();
+
+            return Convert.ToInt32(new string(day));
+        }
+        else
+        {
+            return number;
+        }
     }
 
     static void Main()
