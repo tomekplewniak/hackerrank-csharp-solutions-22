ccb76a396140c44656a7a87b35573e03eee82019
Tomek Plewniak <tomekplewniak@gmail.com>
2021-03-07 11:19:35 +0100

    Setup a test case.

--- ProblemSolving/Implementation/FairRations/Program.cs
+++ ProblemSolving/Implementation/FairRations/Program.cs
@@ -1,40 +1,17 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Solution
 {
-
-    // Complete the fairRations function below.
-    static int fairRations(int[] B)
+    static string FairRations(int[] b)
     {
-
 
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        int[] b = new int[] { 2, 3, 4, 5, 6 };
 
-        int N = Convert.ToInt32(Console.ReadLine());
-
-        int[] B = Array.ConvertAll(Console.ReadLine().Split(' '), BTemp => Convert.ToInt32(BTemp))
-        ;
-        int result = fairRations(B);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
+        var result = FairRations(b);
+        Console.WriteLine(result);
     }
 }
