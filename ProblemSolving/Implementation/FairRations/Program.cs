bbadbfc347521b5063983a6fdefdcd9e792fbe57
Tomek Plewniak <tomekplewniak@gmail.com>
2021-03-07 11:19:56 +0100

    Solve the problem.

--- ProblemSolving/Implementation/FairRations/Program.cs
+++ ProblemSolving/Implementation/FairRations/Program.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Linq;
 
 class Solution
 {
     static string FairRations(int[] b)
     {
+        int oddNumbers = b.Select(c => c % 2 == 0 ? 0 : 1).Sum();
 
+        if (oddNumbers % 2 == 1)
+        {
+            return "NO";
+        }
+
+        int breadNum = 0;
+        for (int i = 0; i < b.Length - 1; i++)
+        {
+            if (b[i] % 2 == 1)
+            {
+                b[i] += 1;
+                b[i + 1] += 1;
+                breadNum += 2;
+            }
+        }
+        return breadNum.ToString();
     }
 
     static void Main()
