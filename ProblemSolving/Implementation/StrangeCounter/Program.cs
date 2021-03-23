a649969b4572437a163c24f599813c10b941884f
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2021-03-23 14:12:29 +0100

    Problem: Strange Counter (#32)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ ProblemSolving/Implementation/StrangeCounter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Solution
+{
+    static long StrangeCounter(long t)
+    {
+        long initialValue = 3;
+        while (initialValue <= t + 2)
+        {
+            initialValue *= 2;
+        }
+
+        return initialValue - t - 2;
+    }
+
+    static void Main()
+    {
+        long t = 4;
+
+        long result = StrangeCounter(t);
+
+        Console.WriteLine(result);
+    }
+}
