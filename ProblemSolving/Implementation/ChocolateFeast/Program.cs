d1aa06cebbdb8888c9e6d46d7928aa7788d6c38a
Tomek Plewniak <tomekplewniak@gmail.com>
2021-10-27 20:19:35 +0200

    Solve the problem.

--- ProblemSolving/Implementation/ChocolateFeast/Program.cs
+++ ProblemSolving/Implementation/ChocolateFeast/Program.cs
@@ -1,61 +1,36 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Result
 {
-
-    /*
-     * Complete the 'chocolateFeast' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. INTEGER n
-     *  2. INTEGER c
-     *  3. INTEGER m
-     */
-
-    public static int chocolateFeast(int n, int c, int m)
+    public static int ChocolateFeast(int n, int c, int m)
     {
-
+        int chocolates = n / c;
+        int answer = chocolates + ChocolateRec(chocolates, m);
+        return answer;
     }
 
+    static int ChocolateRec(int chocolates, int m)
+    {
+
+        if (chocolates < m) return 0;
+        return 1 + ChocolateRec(chocolates - m + 1, m);
+    }
 }
 
 class Solution
 {
     public static void Main(string[] args)
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        // n: Bobby's initial amount of money
+        int n = 15;
 
-        int t = Convert.ToInt32(Console.ReadLine().Trim());
+        // c: the cost of a chocolate bar
+        int c = 3;
 
-        for (int tItr = 0; tItr < t; tItr++)
-        {
-            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+        // m: the number of wrappers he can turn in for a free bar
+        int m = 2;
 
-            int n = Convert.ToInt32(firstMultipleInput[0]);
-
-            int c = Convert.ToInt32(firstMultipleInput[1]);
-
-            int m = Convert.ToInt32(firstMultipleInput[2]);
-
-            int result = Result.chocolateFeast(n, c, m);
-
-            textWriter.WriteLine(result);
-        }
-
-        textWriter.Flush();
-        textWriter.Close();
+        int result = Result.ChocolateFeast(n, c, m);
+        Console.WriteLine(result);
     }
 }
